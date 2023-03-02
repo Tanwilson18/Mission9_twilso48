d6d248cb10ed72153404255a88a1155b36ace9a3
Tanner Wilson <twilso48@byu.edu>
2023-03-02 12:49:54 -0700

    applying bootstrap

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -25,11 +25,18 @@ namespace Mission9_twilso48.Models.Infrastructure
 
         [ViewContext]
         [HtmlAttributeNotBound]
+
+
+
         public ViewContext vc { get; set; }
 
         //different than view context
         public PageInfo pageBlah { get; set;}
         public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
         public override void Process(TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
@@ -41,6 +48,12 @@ namespace Mission9_twilso48.Models.Infrastructure
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == pageBlah.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
