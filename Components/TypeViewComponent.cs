4b673ded3d0ac855a1fd61fd4721332060fc13d9
Tanner Wilson <twilso48@byu.edu>
2023-03-07 22:49:31 -0700

    components

---
+++ Components/TypeViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mission9_twilso48.Models;
+
+namespace Mission9_twilso48.Components
+{
+    public class TypeViewComponent : ViewComponent
+    {
+        private IBookstoreRepo repo { get; set; }
+
+        //construtor 
+        public TypeViewComponent (IBookstoreRepo temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var types = repo.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x=> x);
+
+            return View(types);
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,13 +18,14 @@ namespace Mission9_twilso48.Controllers
         }
         
         //displays the amount of results per page
-        public IActionResult Index(int pageNum = 1)
+        public IActionResult Index(string bookType, int pageNum = 1)
         {
             int pageSize = 10;
 
             var x = new BooksViewModel
             {
                 Books = repo.Books
+                .Where(p => p.Category == bookType)
                 .OrderBy(b => b.Title)
                 .Skip((pageNum - 1) * pageSize)
                 .Take(pageSize),
